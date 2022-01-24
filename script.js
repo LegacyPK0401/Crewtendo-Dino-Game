7cc1812c47cc38df8519dfa64e396c019a88fd9c
PK <kaptai1984@gmail.com>
2022-01-23 18:59:23 -0800

    Finished all animations. Just needs more customization.

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const member = document.getElementById("member");
+const block = document.getElementById("block");
 
 function jump() {
   if (member.classList != "jump") {
     member.classList.add("jump");
   }
-  member.classList.add("jump");
 
   setTimeout(function() {
     member.classList.remove("jump");
   }, 300);
 }
+
+let isAlive = setInterval(function () {
+    //get current member Y position
+    let memberTop = parseInt(window.getComputedStyle(member).getPropertyValue("top"));
+
+    //get current block X position
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+
+    //detect collision
+    if (blockLeft < 50 && blockLeft > 0 && memberTop >= 140) {
+
+      //collision
+      alert("Game Over!");
+
+      block.classList.add("reset");
+
+    setTimeout(function() {
+      block.classList.remove("reset");
+    }, 100);
+    }
+
+}, 10);
 
 document.addEventListener("keydown", function (event) {
   jump();
